28c84e196512d9641f1e9e8d1892e16d36674cec
Dan Odobescu <dan.odobescu@gmail.com>
2017-05-14 19:27:17 +0300

    Test Commit

--- Proiect/Proiect/Proiect/Program.cs
+++ Proiect/Proiect/Proiect/Program.cs
@@ -8,8 +8,9 @@ namespace Proiect
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
+
+        /// <summary> 
+        /// The main entry point for the application
         /// </summary>
         [STAThread]
         static void Main()
